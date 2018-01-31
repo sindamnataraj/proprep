db621e3a4fae1b92148b12457ccc7440fb9eabf6
Nataraj Sindam <nasindam@microsoft.com>
2018-01-31 12:42:16 -0800

    update

---
+++ proprac/Trie.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proprac
+{
+    public class Trie
+    {
+        public class TrieNode {
+            public bool IsEnd;
+            public TrieNode[] Child;
+
+            public TrieNode()
+            {
+                IsEnd = false;
+                Child = new TrieNode[26];
+            }
+        }
+
+
+        private TrieNode Head;
+
+        public Trie()
+        {
+            Head = null;
+        }
+
+
+        public void Insert(string s)
+        {
+            Head = Insert(Head, s, 0);
+        }
+
+        private static TrieNode Insert(TrieNode Head, string s, int i)
+        {
+            if (Head == null)
+            {
+                Head = new TrieNode();
+            }
+
+            if (i == s.Length)
+            {
+                Head.IsEnd = true;
+                return Head;
+            }
+            int index = (int)s[i] - 97;
+
+            Head.Child[index] = Insert(Head.Child[index], s, i + 1);
+            return Head;
+        }
+
+
+        public void PrintAll()
+        {
+            List<string> result = new List<string>();
+            PrintTrie(Head, new StringBuilder(), ref result);
+
+            foreach (var item in result)
+            {
+                Console.Write("{0} ",item);
+            }
+            Console.WriteLine();
+        }
+
+        private static void PrintTrie(TrieNode head, StringBuilder sb,ref List<string> result)
+        {
+            if (head == null) return;
+
+            if (head.IsEnd == true) result.Add(sb.ToString());
+            for (int i = 0; i < 26; i++)
+            {
+                char c = (char)(i + 97);
+                if (head.Child[i] != null)
+                {
+                    StringBuilder newSb = new StringBuilder();
+                    newSb.Append(sb);
+                    newSb.Append(c);
+                    PrintTrie(head.Child[i], newSb, ref result);
+                }
+            }
+        }
+    }
+}
---
+++ proprac/stringproblems.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace proprac
+{
+    public class stringproblems
+    {
+
+        public static string Reverse(string s)
+        {
+            StringBuilder sb = new StringBuilder(s);
+            Reverse(ref sb, 0, s.Length - 1);
+            return sb.ToString();
+        }
+
+        public static void Reverse(ref StringBuilder sb, int i, int j)
+        {
+            while (i < j)
+            {
+                char c = sb[i];
+                sb[i] = sb[j];
+                sb[j] = c;
+
+                i++;
+                j--;
+            }
+        }
+
+
+
+        public static void ReverseSentenceWords(ref StringBuilder sb)
+        {
+            int i = 0;
+            int j = 0;
+
+            while (j < sb.Length)
+            {
+                while ( j < sb.Length && sb[j] != ' ')
+                {
+                    j++;
+                }
+
+                Reverse(ref sb, i, j - 1);
+
+                if (j < sb.Length)
+                {
+                    i = j + 1;
+                    j++;
+                }
+            }
+            
+
+        }
+    }
+}
